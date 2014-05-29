81ae33726de18313b2f38a2ddfabb0d13ab8a8e9
Bruno Alassia <vruno.alassia@gmail.com>
2014-05-28 23:02:19 -0300

    Update Utils.java
    Add md5 static method

--- src/com/urucas/utils/Utils.java
+++ src/com/urucas/utils/Utils.java
@@ -65,6 +65,32 @@ public abstract class Utils {
   	return null;
  	}	
 
+	public static String md5(final String s) {
+
+        	final String MD5 = "MD5";
+        	try {
+            		// Create MD5 Hash
+            		MessageDigest digest = java.security.MessageDigest.getInstance(MD5);
+            		digest.update(s.getBytes());
+            		byte messageDigest[] = digest.digest();
+
+            		// Create Hex String
+            		StringBuilder hexString = new StringBuilder();
+            		for (byte aMessageDigest : messageDigest) {
+                		String h = Integer.toHexString(0xFF & aMessageDigest);
+                		while (h.length() < 2)
+                    			h = "0" + h;
+                		hexString.append(h);
+            		}
+            		return hexString.toString();
+
+        	} catch (NoSuchAlgorithmException e) {
+            		e.printStackTrace();
+        	}
+        	return "";
+    	}
+
+
 	public static int intIDFromResource(Context context, String name, String type) {
 		return context.getResources().getIdentifier(name, type, context.getPackageName());		
 	}
