163b7fc5cac452a92212234d9719fe00504eede0
Bruno Alassia <vruno.alassia@gmail.com>
2014-06-02 20:39:50 -0300

    Update Utils.java
    Add file read methods, convert image resources into to file to upload to a server

--- src/com/urucas/utils/Utils.java
+++ src/com/urucas/utils/Utils.java
@@ -38,6 +38,140 @@ import android.widget.Toast;
 
 public abstract class Utils {
 
+	public static String getRealPathFromUri(Context context, Uri contentUri) {	
+		Log.i("uri", contentUri.toString());
+		if(Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+		    Cursor cursor = null;
+		    try {
+
+		    	String[] proj = { MediaStore.Images.Media.DATA };
+		    	cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+		    	int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+		    	cursor.moveToFirst();
+		    	return cursor.getString(column_index);
+		    } finally {
+		        if (cursor != null) {
+		            cursor.close();
+		        }
+		    }
+		}else{
+			Uri uri = contentUri;
+			// DocumentProvider
+		    if (DocumentsContract.isDocumentUri(context, uri)) {
+		        // ExternalStorageProvider
+		        if (isExternalStorageDocument(uri)) {
+		            final String docId = DocumentsContract.getDocumentId(uri);
+		            final String[] split = docId.split(":");
+		            final String type = split[0];
+
+		            if ("primary".equalsIgnoreCase(type)) {
+		                return Environment.getExternalStorageDirectory() + "/" + split[1];
+		            }
+
+		            // TODO handle non-primary volumes
+		        }
+		        // DownloadsProvider
+		        else if (isDownloadsDocument(uri)) {
+
+		            final String id = DocumentsContract.getDocumentId(uri);
+		            final Uri contentUri2 = ContentUris.withAppendedId(
+		                    Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+		            return getDataColumn(context, contentUri2, null, null);
+		        }
+		        // MediaProvider
+		        else if (isMediaDocument(uri)) {
+		            final String docId = DocumentsContract.getDocumentId(uri);
+		            final String[] split = docId.split(":");
+		            final String type = split[0];
+
+		            Uri contentUri2 = null;
+		            if ("image".equals(type)) {
+		                contentUri2 = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+		            } else if ("video".equals(type)) {
+		                //contentUri2 = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+		            } else if ("audio".equals(type)) {
+		                //contentUri2 = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+		            }
+
+		            final String selection = "_id=?";
+		            final String[] selectionArgs = new String[] {
+		                    split[1]
+		            };
+
+		            return getDataColumn(context, contentUri2, selection, selectionArgs);
+		        }
+		    }
+		    // MediaStore (and general)
+		    else if ("content".equalsIgnoreCase(uri.getScheme())) {
+		        return getDataColumn(context, uri, null, null);
+		    }
+		    // File
+		    else if ("file".equalsIgnoreCase(uri.getScheme())) {
+		        return uri.getPath();
+		    }
+
+		    return null;
+		}
+	}
+	/* metodos copiados */
+	/**
+	 * Get the value of the data column for this Uri. This is useful for
+	 * MediaStore Uris, and other file-based ContentProviders.
+	 *
+	 * @param context The context.
+	 * @param uri The Uri to query.
+	 * @param selection (Optional) Filter used in the query.
+	 * @param selectionArgs (Optional) Selection arguments used in the query.
+	 * @return The value of the _data column, which is typically a file path.
+	 */
+	public static String getDataColumn(Context context, Uri uri, String selection,
+	        String[] selectionArgs) {
+
+	    Cursor cursor = null;
+	    final String column = "_data";
+	    final String[] projection = {
+	            column
+	    };
+
+	    try {
+	        cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+	                null);
+	        if (cursor != null && cursor.moveToFirst()) {
+	            final int column_index = cursor.getColumnIndexOrThrow(column);
+	            return cursor.getString(column_index);
+	        }
+	    } finally {
+	        if (cursor != null)
+	            cursor.close();
+	    }
+	    return null;
+	}
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is ExternalStorageProvider.
+	 */
+	public static boolean isExternalStorageDocument(Uri uri) {
+	    return "com.android.externalstorage.documents".equals(uri.getAuthority());
+	}
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is DownloadsProvider.
+	 */
+	public static boolean isDownloadsDocument(Uri uri) {
+	    return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+	}
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is MediaProvider.
+	 */
+	public static boolean isMediaDocument(Uri uri) {
+	    return "com.android.providers.media.documents".equals(uri.getAuthority());
+	}
+
 	public static String generateNonce() {
   		try {
     			// Create a secure random number generator
